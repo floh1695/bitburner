f2a9f0272cc9b158f60a781f1e6e359502900609
John Brock <14806462+JohnLRBrock@users.noreply.github.com>
2022-04-15 14:19:50 -0400

    updated server purchasing script
    You can now target servers with the script you're running.

--- dumped/john/player-j1.js
+++ dumped/john/player-j1.js
@@ -2,12 +2,12 @@
  * @param {NS} ns
  * @return {Promise<Void>}
  **/
-export const main = async (ns) => {};
+export const main = (ns) => {};
 
 /** 
  * @param {NS} ns
  * @return {Number}
  **/
-export const getPlayerMoney = async (ns) => {
+export const getPlayerMoney = (ns) => {
 	return ns.getServerMoneyAvailable('home');
 };
