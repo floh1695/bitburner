037f7ace197e0b2ea8005f6fe7639c3c4c430eb5
Charles Bayley <cbayley95@gmail.com>
2022-04-20 21:32:15 -0400

    Cleaned up and found out wget caches its results until refresh

--- server/src/scripts/orchestrator.ts
+++ server/src/scripts/orchestrator.ts
@@ -1,20 +1,24 @@
 import * as nsd from "../../external/NetscriptDefinitions";
 
+const urlBase = 'http://localhost:8088';
+const scriptUrlBase = urlBase + '/scripts';
+const scriptUrlFor = (script: string): string =>
+  `${scriptUrlBase}/${script}`;
+
+const scriptsFile = 'scripts.json.txt';
+
 /**
  * This file cannot depend on other scripts since it will be the first one deployed
  */
 export const main = async (ns: nsd.NS) => {
-  const scriptsPath = 'http://localhost:8088/scripts';
-  const scriptsFile = 'scripts.json.txt';
-  
-  await ns.wget(scriptsPath, scriptsFile);  // TODO: For some reason this fails to write to the file
-  const scriptsRaw = ns.read(scriptsFile);
-  ns.tprint(scriptsRaw);
-  const rawScripts = JSON.parse(scriptsRaw) as string[];
-  const scripts = rawScripts.map(s => `./${s}`);
-  for (const script of scripts) {
-    const url = `http://localhost:8088/scripts/${script}`;
+  ns.wget(scriptUrlBase, scriptsFile);
 
-    await ns.wget(url, script);
+  const scriptsJson = ns.read(scriptsFile);
+  const scripts = JSON.parse(scriptsJson);
+
+  for (const script of scripts) {
+    const url = scriptUrlFor(script);
+
+    ns.wget(url, script);
   }
 };
